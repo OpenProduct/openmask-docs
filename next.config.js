231d0ba822c26922f16f9f8127649a867edd7584
Nikita Kuznetsov <nkuz915@gmail.com>
2022-08-24 21:07:48 +0200

    Update config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withNextra = require("nextra")({
 
 module.exports = withNextra({
   i18n: {
-    locales: ["en-US", "zh-CN", "es-ES", "pt-BR", "ja", "ko", "ru"],
+    locales: ["en-US"],
     defaultLocale: "en-US",
   },
   redirects: () => {
