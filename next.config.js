35745df0ddacd50c4d08be3a4628beaafa6b27d8
Nikita Kuznetsov <nkuz915@gmail.com>
2022-08-30 23:11:24 +0200

    Remove Examples

--- next.config.js
+++ next.config.js
@@ -18,39 +18,14 @@ module.exports = withNextra({
         statusCode: 301,
       },
       {
-        source: "/advanced/performance",
-        destination: "/docs/advanced/performance",
-        statusCode: 301,
-      },
-      {
-        source: "/advanced/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/docs/cache",
-        destination: "/docs/advanced/cache",
-        statusCode: 301,
-      },
-      {
-        source: "/change-log",
-        destination: "/docs/change-log",
-        statusCode: 301,
-      },
-      {
         source: "/blog/swr-1",
         destination: "/blog/swr-v1",
         statusCode: 301,
       },
       {
-        source: "/docs",
-        destination: "/docs/getting-started",
-        statusCode: 302,
-      },
-      {
-        source: "/examples",
-        destination: "/examples/basic",
-        statusCode: 302,
+        source: "/docs/api-reference",
+        destination: "/docs/api-reference/ton-provider",
+        statusCode: 301,
       },
     ];
   },
