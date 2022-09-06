999b813f8481868fdba639b2452ccf721d04c8ed
Nikita Kuznetsov <nkuz915@gmail.com>
2022-09-06 11:55:16 +0200

    Switch network example

--- components/example/switch.js
+++ components/example/switch.js
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 import Callout from "nextra-theme-docs/callout";
 
-function getProvider() {
-  const provider = window.ton;
-  if (!provider || !provider.isOpenMask) {
-    throw new Error("Please install OpenMask");
-  }
-  return provider;
-}
-
 export default () => {
   const [network, setNetwork] = useState("");
+
+  const switchNetwork = async () => {
+    const provider = window.ton ?? {};
+    if (!provider?.isOpenMask) return;
+
+    const next = network === "mainnet" ? "testnet" : "mainnet";
+    try {
+      await provider.send("wallet_switchChain", next);
+
+      setNetwork(next);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   useEffect(() => {
     const provider = window.ton ?? {};
@@ -18,17 +24,16 @@ export default () => {
 
     // Initial chainId value
     provider.send("ton_getChain").then((chainId) => setNetwork(chainId));
-
-    // Subscribe to change network
-    provider.on("chainChanged", setNetwork);
-    return () => {
-      // Unsubscribe
-      provider.off("chainChanged", setNetwork);
-    };
   }, []);
 
   return (
     <div className="pb-8 pt-4">
+      <button
+        onClick={switchNetwork}
+        className="py-3 px-5 mb-3 bg-[#88d3ff] text-black text-lg"
+      >
+        Switch network
+      </button>
       {network ? (
         <Callout emoji={network === "mainnet" ? "💎" : "⚙️"}>{network}</Callout>
       ) : (
