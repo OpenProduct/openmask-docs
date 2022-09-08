c4365000bde2144924775e629ac825dcef6cad57
Nikita Kuznetsov <nkuz915@gmail.com>
2022-09-08 23:09:08 +0200

    Update index page

--- components/example.js
+++ components/example.js
@@ -1,11 +1,43 @@
-export default () => {
+export const Example1 = () => {
   return (
     <img
       src="/TonMaskExample.png"
       width="250px"
       style={{
-        margin: "20px 0 0 20px",
-        transform: "rotate3d(1, 2, 0, 316deg)",
+        margin: "20px 0 20px 20px",
+        // transform: "rotate3d(1, 2, 0, 316deg)",
+        float: "right",
+        padding: "5px",
+        background: "gray",
+      }}
+    />
+  );
+};
+
+export const Example2 = () => {
+  return (
+    <img
+      src="/TonMaskExample2.png"
+      width="250px"
+      style={{
+        margin: "40px 40px 20px 0",
+        // transform: "rotate3d(1, 2, 0, 316deg)",
+        float: "left",
+        padding: "5px",
+        background: "gray",
+      }}
+    />
+  );
+};
+
+export const Example3 = () => {
+  return (
+    <img
+      src="/TonMaskExample3.png"
+      width="250px"
+      style={{
+        margin: "0 20px 20px 0",
+        // transform: "rotate3d(1, 2, 0, 316deg)",
         float: "right",
         padding: "5px",
         background: "gray",
