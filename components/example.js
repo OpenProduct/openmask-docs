79ef0a62a72e77a28798f58af02a6edeaee4f2d1
Nikita Kuznetsov <nkuz915@gmail.com>
2022-09-08 23:12:27 +0200

    Add examples to permission page

--- components/example.js
+++ components/example.js
@@ -33,10 +33,26 @@ export const Example2 = () => {
 export const Example3 = () => {
   return (
     <img
+      src="/TonMaskExample2.png"
+      width="250px"
+      style={{
+        margin: "50px 0 20px 20px",
+        // transform: "rotate3d(1, 2, 0, 316deg)",
+        float: "right",
+        padding: "5px",
+        background: "gray",
+      }}
+    />
+  );
+};
+
+export const Example4 = () => {
+  return (
+    <img
       src="/TonMaskExample3.png"
       width="250px"
       style={{
-        margin: "0 20px 20px 0",
+        margin: "20px 0 20px 20px",
         // transform: "rotate3d(1, 2, 0, 316deg)",
         float: "right",
         padding: "5px",
